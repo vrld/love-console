cdf11329947ba15e943348a3fc84c3232e2da65c
Matthias Richter <matthias@dsboard.de>
2011-01-28 15:19:53 +0100

    Catch errors to avoid BSOD

--- main.lua
+++ main.lua
@@ -1,58 +1,37 @@
 local _print = print
-local IN =require 'input'
-local OUT = require 'output'
-
-input = IN:new()
-local output
+local Console = require "console"
+console = nil
+input   = nil
+output  = nil
 
 local console_active = true
 
-local chunk = nil
-prompt1, prompt2 = "> ", "| "
-local prompt = prompt1
-function input:onCommand(cmd)
-	output:push(prompt, cmd)
-	cmd = cmd:gsub("^=%s?", "return "):gsub("^return%s+(.*)(%s*)$", "print(%1)%2")
-	chunk = chunk and table.concat({chunk, cmd}, " ") or cmd
-	local ok, out = pcall(function() assert(loadstring(chunk))() end)
-	if not ok and out:match("'<eof>'") then
-		prompt = prompt2
-		input.history[#input.history] = nil
-	else
-		prompt = prompt1
-		if out and out:len() > 0 then
-			output:push(out)
-		end
-		input.history[#input.history] = chunk
-		chunk = nil
-	end
+local debug = debug
+local function error_handler(msg)
+	print((debug.traceback("Error: " .. msg, 4):gsub("\t", "    ")))
 end
-input.complete_add_parens = true
-input.complete_base = _G
 
-local function draw()
-	love.graphics.setColor(34,34,34,180)
-	love.graphics.rectangle('fill', 2,2, love.graphics.getWidth()-4, love.graphics.getHeight()-4)
-	love.graphics.setColor(221,221,221)
-	local inp = table.concat{prompt, input:current(), " "}
-	local n = output:push(inp)
-	output:draw(4, love.graphics.getHeight() - 4, input:pos())
-	output:pop(n)
-end
+local handler_names = {
+	kr = 'keyreleased',
+	mp = 'mousepressed',
+	mr = 'mousereleased',
+	jp = 'joystickpressed',
+	jr = 'joystickreleased',
+	f = 'focus',
+}
 
 function love.run()
-	love.graphics.setFont('VeraMono.ttf', 14)
 	love.keyboard.setKeyRepeat(150, 50)
 	love.graphics.setBackgroundColor(34,34,34)
-
-	output = OUT.new()
+	local font = love.graphics.newFont('VeraMono.ttf', 14)
+	console = Console.new(font)
 
 	print("|  '/\\'\\  / /_\\  __  /' /\\ |\\ | /_ /\\ |   /_\\")
 	print("|__ \\/  \\/  \\_       \\, \\/ | \\|  / \\/ |__ \\_   (v.1e-127)")
 	print()
 	print("<Escape> toggles the console. Call quit() or exit() to quit.")
 	print("Try hitting <Tab> to complete your current input.")
-	print("You can overwrite every love callback except love.keypressed.")
+	print("You can overwrite every love callback (but love.keypressed).")
 	print()
 
 	local dt = 0
@@ -60,31 +39,55 @@ function love.run()
 		love.timer.step()
 		dt = love.timer.getDelta()
 
-		if love.update then love.update(dt) end
+		if love.update then
+			-- if exists, call love.update. on error, remove function
+			if not xpcall(function() love.update(dt) end, error_handler) then
+				love.update = nil
+			end
+		end
+
 		love.graphics.clear()
-		if love.draw then love.draw() end
+		if love.draw then
+			-- if exists, call love.draw. on error, remove function
+			if not xpcall(love.draw, error_handler) then
+				love.draw = nil
+			end
+		end
+
+		-- draw console
 		if console_active then
 			local color = {love.graphics.getColor()}
-			assert(pcall(draw))
+			love.graphics.setColor(34,34,34,180)
+			love.graphics.rectangle('fill', 2,2, love.graphics.getWidth()-4, love.graphics.getHeight()-4)
+			love.graphics.setColor(221,221,221)
+			console:draw(4, love.graphics.getHeight() - 4)
 			love.graphics.setColor(unpack(color))
 		end
 
+		-- process events
 		for e,a,b,c in love.event.poll() do
 			if e == "q" then
+				-- quit event
 				if not love.quit or not love.quit() then
 					love.audio.stop()
 					return
 				end
 			elseif e == "kp" then
+				-- keypress event. let us catch that
 				if a == 'escape' then
 					console_active = not console_active
 				end
 
 				if console_active then
-					input:keypressed(a,b)
+					console:keypressed(a,b)
 				end
 			else
-				love.handlers[e](a,b,c)
+				-- call handlers in protective mode. if they fail,
+				-- show error and remove the handler
+				local f = love[ handler_names[e] ]
+				if type(f) == "function" and not xpcall(function() f(a,b,c) end, error_handler) then
+					love[ handler_names[e] ] = nil
+				end
 			end
 		end
 
@@ -94,12 +97,11 @@ function love.run()
 end
 
 function print(...)
-	local n_args, s = select('#', ...), {...}
-	for i = 1,n_args do
-		s[i] = (s[i] == nil) and "nil" or tostring(s[i])
-	end
-	if n_args == 0 then s = {" "} end
-	output:push(table.concat(s, "    "))
+	return console:print(...)
+end
+
+function printf(fmt, ...)
+	return print(string.format(fmt, ...))
 end
 
 function quit()
